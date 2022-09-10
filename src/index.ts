b0acdec59b59fb5a5f44ec00e53e5aef94fb057a
simonljus <simon.ljus94@gmail.com>
2022-09-10 12:52:09 +0200

    updating existing pages in database

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,21 @@ import 'dotenv/config'
 import * as natural from 'natural';
 import fetch from "cross-fetch";
 import { APIErrorCode, APIResponseError, Client } from "@notionhq/client";
-import { PageObjectResponse, QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
+import { PageObjectResponse, QueryDatabaseResponse, updatePage, UpdatePageParameters } from "@notionhq/client/build/src/api-endpoints";
 import { Presets, SingleBar } from "cli-progress";
 import { readFile } from 'fs/promises';
 const notion = new Client({ auth: process.env.NOTION_KEY });
 const databaseId = process.env.NOTION_DATABASE_ID as string;
 interface Company {
-  cities: Array<string>;
-  text: string;
+  cities: Set<string>;
+  description: string;
   name: string;
-  url: string;
+  link: string | null;
   raw: string;
-  tags: Array<string>
+  tags: Set<string>
+}
+interface CompanyPage extends Company{
+  pageId:string
 }
 async function getTags(){
     const stemMap = new Map<string,Set<string>>()
@@ -83,32 +86,96 @@ async function scrapeCompanies() {
         tags.push(...tokenTags)
       })
       return {
-        url: n.getAttribute("href") as string,
+        link: n.getAttribute("href") || null,
         name: n.textContent,
-        text,
-        cities: cities.filter(c=> !!c),
+        description:text,
+        cities: new Set<string>(cities.map(c=> c?.trim()).filter(c=> !!c)),
         raw,
-        tags: Array.from(new Set<string>(tags)).filter(t=>!!t)
+        tags: new Set<string>(tags.map(t=> t?.trim()).filter(t=>!!t))
       };
     })
     .slice(3);
     return companies;
 }
-async function addCompaniesToNotion(companies:Array<Company>,exclude: Set<string>){
+function mapBy<K, V>(items: V[], getKey: (item: V) => K): Map<K, V> {
+	const m = new Map();
+	items?.forEach((item) => {
+		const key = getKey(item);
+		if (!m.has(key)) {
+			m.set(key, item);
+		}
+	});
+	return m;
+}
+async function addCompaniesToNotion({companies, existing}: {companies: Array<Company>, existing: Array<CompanyPage>}){
   const bar1 = new SingleBar({},Presets.shades_classic);
   const createdPages = new Array<string>()
+  const updatedPages = new Array<string>()
+  const unchangedPages = new Array<string>()
+  const existingMap = mapBy(existing,e=> e.name)
   bar1.start(companies.length,0)
     for( const company of companies){
-      if(!exclude.has(company.name)){
+      const companyPage = existingMap.get(company.name)
+      if(companyPage){
+        const updatedPageId = await updateCompanyPage(company,companyPage)
+        if(updatedPageId){
+          updatedPages.push(updatedPageId)
+        }else{
+          unchangedPages.push(companyPage.pageId)
+        }
+      }
+      else{
         const pageId = await addCompanyToPage(company)
         if(pageId){
           createdPages.push(pageId)
         }
-       
       }
       bar1.increment()
     }
-    return createdPages
+    return {created: createdPages,updated:updatedPages,unchanged: unchangedPages}
+}
+function areSetsEqual(aItems: Set<string>,bItems:Set<string>):boolean{
+return aItems.size === bItems.size &&
+    [...aItems].every((item) => bItems.has(item));
+}
+async function updateCompanyPage(company: Company, companyPage: CompanyPage) {
+  let  changedProps  = false;
+  const args: Required<Pick<UpdatePageParameters,'page_id' | 'properties'>> = {page_id: companyPage.pageId, properties: {}}
+  const diffs : Array<string> = []
+  if(!areSetsEqual(company.cities,companyPage.cities)){
+    args.properties.Cities = {multi_select: Array.from(company.cities).map(c=> ({name:c}))}
+    diffs.push(`Cities: ${Array.from(company.cities).sort()}, ${Array.from(companyPage.cities).sort()} ` )
+     changedProps=true
+  }
+  if(!areSetsEqual(company.tags,companyPage.tags)){
+    args.properties.Tags = {multi_select: Array.from(company.tags).map(t=> ({name: t}))}
+    diffs.push(`Name: ${Array.from(company.tags).sort()}, ${Array.from(companyPage.tags).sort()}`)
+    changedProps=true
+ }
+  if(company.raw  !== companyPage.raw){
+    args.properties.Raw = {rich_text: [{text: {content: company.raw}}]}
+    diffs.push(`Raw: ${company.raw}, ${companyPage.raw}`)
+    changedProps=true
+  }
+  if(company.description  !== companyPage.description){
+    args.properties.Description= {rich_text: [{text: {content: company.description}}]}
+    diffs.push(`Description: ${company.description}, ${companyPage.description}`, )
+    changedProps=true
+  }
+  if(company.link  !== companyPage.link){
+    args.properties.Link = {url: company.link}
+    diffs.push(`Link: ${company.link}, ${companyPage.link}`, )
+    changedProps=true
+  }
+  if(diffs.length){
+    console.log(`\n Diffs ${company.name}`)
+    console.log(diffs.join("\n"))
+    console.log(`\n`)
+  }
+  if(changedProps){
+    const p = await notion.pages.update(args)
+    return p.id
+  }  
 }
 async function addCompanyToPage(company: Company) {
     try {
@@ -116,11 +183,11 @@ async function addCompanyToPage(company: Company) {
             parent: { database_id: databaseId },
             properties: { 
                 "Name": { title: [{ text: { content: company.name } }] },
-                "Description": {rich_text: [{text: {content: company.text}}]},
-                "Link": {url: company.url} ,
-                "Cities": {multi_select: company.cities.map(c=> ({name:c.trim()}))},
+                "Description": {rich_text: [{text: {content: company.description}}]},
+                "Link": {url: company.link} ,
+                "Cities": {multi_select: Array.from(company.cities).map(c=> ({name:c}))},
                 "Raw": {rich_text: [{text: {content: company.raw}}]},
-                "Tags": {multi_select: company.tags.map(t=> ({name: t}))}
+                "Tags": {multi_select: Array.from(company.tags).map(t=> ({name: t}))}
             },
           });
           return resp.id
@@ -188,11 +255,11 @@ async function getExistingPageNames(): Promise<Array<string>>{
  const companies = await getExistingPages()
   return companies.map(c=>c.name)
 }
-async function getExistingPages():Promise<Array<{name:string}>>{
+async function getExistingPages():Promise<Array<CompanyPage>>{
   let startCursor: string | null  = null;
-  const allCompanies: Array<{name: string}> = [] 
+  const allCompanies: Array<CompanyPage> = [] 
   do{
-    const queryResponse: QueryDatabaseResponse = await notion.databases.query({database_id: databaseId,page_size:100,start_cursor: startCursor || undefined})
+    const queryResponse: QueryDatabaseResponse = await notion.databases.query({database_id: databaseId,page_size:100,start_cursor: startCursor || undefined, sorts:[{property:'Name',direction:'ascending'}]})
     startCursor = queryResponse.next_cursor;
     const companies = queryResponse.results.map(r=>  pageToCompany(r as PageObjectResponse))
     allCompanies.push(...companies)
@@ -201,18 +268,46 @@ async function getExistingPages():Promise<Array<{name:string}>>{
   console.log(`found ${new Set(allCompanies.map(c=>c.name)).size} companies `)
   return allCompanies
 }
-function pageToCompany(page: PageObjectResponse): {name: string,tags: Array<string>}{
+function pageToCompany(page: PageObjectResponse): CompanyPage{
   const name = getTitleFromPage(page)
-  const tags = getTagsFromPage(page)
-  return {name,tags}
+  const tags = getMultiSelectNamesFromPage(page,'Tags')
+  const cities = getMultiSelectNamesFromPage(page,'Cities')
+  const link = getURLFromPage(page,'Link')
+  const description = getTextFromPage(page,'Description')
+  const raw = getTextFromPage(page,'Raw')
+  const pageId = page.id
+  return {name,tags,cities,link,description,raw,pageId}
 }
-function getTagsFromPage(page: PageObjectResponse): Array<string>{
-  const nameProp = page.properties.Tags
-    if(nameProp?.type !== 'multi_select'){
-      throw new Error(`Tags prop is not a multiselect ${page.properties}`);
-    }
-    const selections = nameProp?.multi_select.map(s=> s.name)
-    return selections
+function getTextFromPage(page: PageObjectResponse,column: string): string{
+  const prop = page.properties[column]
+  if(!prop){
+    throw new Error(`${column} prop is not found ${page.properties}`);
+  }
+  if(prop.type !== 'rich_text'){
+    throw new Error(`${column} prop is not a rich_text ${page.properties}`);
+  }
+  return prop.rich_text[0]?.plain_text
+}
+function getURLFromPage(page:PageObjectResponse,column:string): string | null{
+  const prop = page.properties[column]
+  if(!prop){
+    throw new Error(`${column} prop is not found ${page.properties}`);
+  }
+  if(prop.type !== 'url'){
+    throw new Error(`${column} prop is not a url ${page.properties}`);
+  }
+  return prop.url
+}
+function getMultiSelectNamesFromPage(page: PageObjectResponse,column: string): Set<string>{
+  const prop = page.properties[column]
+  if(!prop){
+    throw new Error(`${column} prop is not found ${page.properties}`);
+  }
+  if(prop?.type !== 'multi_select'){
+      throw new Error(`${column} prop is not a multiselect ${page.properties}`);
+  }
+  const selections = prop?.multi_select.map(s=> s.name)
+  return new Set<string>(selections)
     
 }
 function getTitleFromPage(page: PageObjectResponse): string{
@@ -234,15 +329,17 @@ function getTitleFromPage(page: PageObjectResponse): string{
 }
 async function main(){
   const companies = await scrapeCompanies();
-  const pageNames = await getExistingPageNames();
-  const createdPages =await addCompaniesToNotion(companies, new Set(pageNames))
-  console.log({created: createdPages.length,scraped: companies.length})
+  const existing = await getExistingPages();
+  const pageIds =await addCompaniesToNotion({companies, existing})
+  console.log("\n Done \n")
+  console.log({created: pageIds.created.length,scraped: companies.length,updated: pageIds.updated.length, unchanged:pageIds.unchanged.length})
 
 }
 async function develop(){
-  const tags = await getTags()
-  //const pages = await getExistingPages()
-  console.log(tags)
+  const pages = await getExistingPages()
+  console.log(pages)
 }
 
 main()
+
+
